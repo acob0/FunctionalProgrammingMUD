d28beb7bc12f7df71462c6be58a1d2f6703a93bc
acob0 <37633534+acob0@users.noreply.github.com>
2018-05-06 15:35:42 +0100

    Object functions added

--- scratch.rkt
+++ scratch.rkt
@@ -4,6 +4,7 @@
 (require srfi/13)
 (require srfi/48)
 
+
 ;Defining which objects are in the game
 (define objects '((1 "a silver dagger")
                   (2 "a gold coin")))
@@ -37,6 +38,75 @@
                         (13 ((north) 15) ((south) 14) ((west) 12) ,@actions)
                         (14 ((west) 13) ,@actions)
                         (15 ((south) 13) ,@actions)))
+
+;Defining the hashes the will hold objects and your inventory
+(define  objectdb (make-hash ))
+(define  inventorydb (make-hash ))
+
+;Defining the function to add an object to a hash
+(define (add-object  db id  object)
+  (if (hash-has-key? db id);                                                               Checks if the hash contains the given key
+      (let (( record (hash-ref  db id)));                                                  #t Returns the value for key in hash and binds it to the variable record
+        (hash-set! db id (cons  object  record )));                                        #t Joins object and record and maps id to that in hash, overwriting any existing mapping for id
+      (hash-set! db id (cons  object  empty ))));                                          #f Joins object and an empty list and maps id to that in hash, overwriting any existing mapping for id
+
+;Defining the function to add the list of objects to a hash 
+(define (add-objects  db)
+  (for-each;                                                                               Applies the following annonymous function to each element of the objects list
+   (lambda (r);                                                                            Create an annonymous function that takes r as its argument
+     (add-object  db (first r) (second r))) objects));                                     Use the add-object function from above to add items to a hash where the first element of the list is the key and the second the value
+
+;Defining a function to display which objects are in a given hash
+(define (display-objects db id)
+  (cond ((hash-has-key? db id);                                                            Check whether the hash contains the given key
+    (let* (( record (hash-ref  db id));                                                    #t Returns the value for key in hash and binds it to the variable record
+           (output (string-join  record " and ")));                                        #t Creates a string with the elements of record seperated by the word and, then sets this to the output variable
+      (cond ((not (equal? output ""));                                                     #t Checks that the output isn't empty
+        (if (eq? id 'bag);                                                                 #t #t Checks if the output equals bag
+            (printf "You  are  carrying ~a.\n" output);                                    #t #t #t Print what you are carring in your bag
+            (printf "You  can  see ~a.\n" output))))));                                    #t #t #f Print what there is in the room
+        (#t (if (eq? id 'bag);                                                             #f If the key can't be found in the given hash and the id equals bag
+            (printf "Your bag is empty.\n");                                               #f #t Print that your bag is empty
+            (printf "The room is empty.\n")))));                                           #f #f Print that the room is empty
+
+;Defining a function that either returns the id or bag
+(define (check a b id)
+  (cond ((equal? a b) 'bag);                                                               If a and b are equal return 'bag
+        (#t id)));                                                                         Else return the id
+
+;Defining a function that removes an item from one has and adds it to the other
+(define (remove-object db id from input)
+  (let*((str (string-join (cdr (string-split input))));                                    Splits the input into seperate strings, removes the first string, then joins it back into one string and stores it as str
+        (newid (check from 'bag id)));                                                     Checks whether from equals 'bag and stores the result ('bag or id) as newid
+    (when (hash-has-key? db newid);                                                        Checks whether the hash given contains the key created
+      (let* ((record (hash-ref db newid));                                                 #t Sets record to be the value paired with the created key
+                 (result (remove (lambda (x) (string-suffix-ci? str x)) record));          #t Checks whether str apears anywhere in x (case insensitive) and removes the first instance of it from record and binds that to result
+                 (item (lset-difference equal? record result)));                           #t Gets all the elements that are in record but not in result and stores that as item
+        (cond ((null? item);                                                               #t Checks whether the item is null
+               (printf "I don't see that item in the ~a! \n" from));                       #t #t If the item is null print that it can't find the item
+              (else
+               (cond((eq? from 'room);                                                     #t #f Checks whether the item is from the room
+                     (printf "Added ~a to your bag.\n" (first item));                      #t #f #t Tells you it added the item to your bag
+                     (add-object inventorydb 'bag (first item));                           #t #f #t Adds the item to the inventory hash
+                     (hash-set! db id result));                                            #t #f #t Maps id to result in the given hash, overwriting any existing record
+                    (else
+                     (printf "Removed ~a from your bag . \n" (first item));                #t #f #f Tells you it removed the item to your bag
+                     (add-object objectdb id (first item));                                #t #f #f Adds the item to the object hash
+                     (hash-set! db 'bag result)))))))));                                   #t #f #t Maps 'bag to result in the given hash, overwriting any existing record
+
+;Defining a function to remove an item from the objectdb hash
+(define (pick-item  id  input)
+  (let ((item (string-join (cdr (string-split  input )))));                                Splits the input up into individual strings then removes the first string and joins it back together into one string and stores it as item
+    (remove-object objectdb id 'room item )));                                             Then uses the remove-object function to remove the item from the objectdb hash and add it to the inventorydb hash
+
+;Defining a function to add an item to your inventory
+(define (put-item  id  input)
+  (let ((item (string-join (cdr (string-split  input )))));                                Splits the input up into individual strings then removes the first string and joins it back together into one string and stores it as item
+    (remove-object inventorydb id 'bag item )));                                           Then uses the remove-object function to remove the item from the inventorydb hash and add it to the objectdb hash
+
+;Defining a function that calls display-objects for the inventory
+(define (display-inventory)
+  (display-objects  inventorydb 'bag))
 
 ;Defining a function to create a single string from a list
 (define (slist->string l)
@@ -121,6 +191,15 @@
                (loop id #f));                                                              
               ((eq? response 'look);                                                       If the response equals look, show the possible directions and loop again
                (get-directions id)
+               (loop id #f))
+              ((eq? response 'inventory);                                                  If the response is inventory, show the inventory and loop again
+               (display-objects inventorydb 'bag)
+               (loop id #f))
+              ((eq? response 'pick);                                                       If the response is pick pick up the item
+               (remove-object objectdb id 'bag input)
+               (loop id #f))
+              ((eq? response 'drop);                                                       If the response is drop drop the item
+               (remove-object inventorydb id 'room input)
                (loop id #f))
               ((eq? response 'help);                                                       If the response is drop drop the item
                (printf "~a\n" (map (lambda (x) slist->string(car x)) (get-keywords id)))
